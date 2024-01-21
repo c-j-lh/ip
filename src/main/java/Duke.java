0f096d257895617f77d1b828f7be269c040d241d
Chieu Le Heng <chieuleheng@gmail.com>
2024-01-21 11:21:50 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,42 +2,71 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    static ArrayList<String> list = new ArrayList<>();
+    static ArrayList<Task> tasks = new ArrayList<>();
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
         String greet = "____________________________________________________________\n" +
                 " Hello! I'm Jamie\n" +
                 " What can I do for you?\n" +
                 "____________________________________________________________\n";
-        String bar="____________________________________________________________";
+        String bar = "____________________________________________________________";
         System.out.println(greet);
 
-        Scanner in = new Scanner(System.in);;
+        Scanner in = new Scanner(System.in);
         while (true) {
-            String inp=in.next();
-            if (inp.equals("bye")) {
-                System.out.println("____________________________________________________________\n" +
-                        "Bye. Hope to see you again soon!\n" +
-                        "____________________________________________________________");
-                break;
+            String input = in.nextLine();
+            String[] parts = input.split(" ", 2);
+            String command = parts[0];
+
+            switch (command) {
+                case "bye":
+                    System.out.println(bar + "\nBye. Hope to see you again soon!\n" + bar);
+                    return;
+                case "list":
+                    System.out.println(bar + "\n Here are the tasks in your list:");
+                    for (int i = 0; i < tasks.size(); i++) {
+                        Task task = tasks.get(i);
+                        System.out.printf("%d.[%s] %s\n", i + 1, task.isMarked() ? "X" : " ", task.getDescription());
+                    }
+                    System.out.println(bar);
+                    break;
+                case "mark":
+                    int markIndex = Integer.parseInt(parts[1]) - 1;
+                    tasks.get(markIndex).setMarked(true);
+                    System.out.println(bar + "\n Nice! I've marked this task as done:\n   [X] " + tasks.get(markIndex).getDescription() + "\n" + bar);
+                    break;
+                case "unmark":
+                    int unmarkIndex = Integer.parseInt(parts[1]) - 1;
+                    tasks.get(unmarkIndex).setMarked(false);
+                    System.out.println(bar + "\n OK, I've marked this task as not done yet:\n   [ ] " + tasks.get(unmarkIndex).getDescription() + "\n" + bar);
+                    break;
+                default:
+                    Task newTask = new Task(parts[0], false);
+                    tasks.add(newTask);
+                    System.out.println(bar + "\n added: " + newTask.getDescription() + "\n" + bar);
             }
-            if (inp.equals("list")) {
-                System.out.println(bar);
-                for (int i = 0; i < list.size(); i++) {
-                    System.out.printf("%d. %s\n", i+1, list.get(i));
-                }
-                System.out.println(bar);
-            } else {
-                list.add(inp);
-                System.out.println(bar);
-                System.out.printf("added: %s\n", inp);
-                System.out.println(bar);
-            }
-            //inp = in.next();
+        }
+    }
+
+    static class Task {
+        private String description;
+        private boolean isMarked;
+
+        public Task(String description, boolean isMarked) {
+            this.description = description;
+            this.isMarked = isMarked;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public boolean isMarked() {
+            return isMarked;
+        }
+
+        public void setMarked(boolean marked) {
+            isMarked = marked;
         }
     }
 }
