0564906539f25aa1ff6389cc5b4f5c0ccb9f537b
Chieu Le Heng <chieuleheng@gmail.com>
2025-08-19 19:37:06 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,6 +14,7 @@ public class Duke {
         String m = "    ____________________________________________________________\r\n";
                         
         ArrayList<String> al = new ArrayList<>();
+        ArrayList<Boolean> al2 = new ArrayList<>();
         Scanner s = new Scanner(System.in);
         System.out.println(logo);
         while(true){
@@ -24,11 +25,29 @@ public class Duke {
             } else if(in.equals("list")) {
                 System.out.print(m);
                 for (int i = 0; i < al.size(); i++) {
-                    System.out.printf("     %d. %s\n", i+1, al.get(i));
+                    System.out.printf("     %d.[%s] %s\n", i+1, 
+                        al2.get(i) ? "X": " ",  al.get(i));
                 }
                 System.out.print(m);
+            } else if(in.split(" ")[0].equals("mark")) {
+                int n = Integer.parseInt(in.split(" ")[1]) - 1;
+                al2.set(n, true);
+                System.out.print(m);
+                System.out.println("     Nice! I've marked this task as done:");
+                System.out.println("       [X] " + al.get(n));
+                System.out.print(m);
+                
+            } else if(in.split(" ")[0].equals("unmark")) {
+                int n = Integer.parseInt(in.split(" ")[1]) - 1;
+                al2.set(n, false);
+                System.out.print(m);
+                System.out.println("     OK, I've marked this task as not done yet:");
+                System.out.println("       [ ] " + al.get(n));
+                System.out.print(m);
+                
             } else {
                 al.add(in);
+                al2.add(false);
                 System.out.print(m);
                 System.out.println("     added: "+in);
                 System.out.print(m);
