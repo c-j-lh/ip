fd2ee6e79c41dca7c06256936f58e802461783fd
Chieu Le Heng <chieuleheng@gmail.com>
2025-08-20 17:45:47 +0800

    A‑Enums

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Duke {
+
+    enum TaskType { T, D, E }
+
     public static void main(String[] args) {
         String logo =   "     Hello! I'm Rama2\n" +
                         "     What can I do for you?\n";
@@ -10,7 +13,7 @@ public class Duke {
 
         ArrayList<String> al = new ArrayList<>();
         ArrayList<Boolean> al2 = new ArrayList<>();
-        ArrayList<Character> typ = new ArrayList<>();
+        ArrayList<TaskType> typ = new ArrayList<>();
         ArrayList<String> det = new ArrayList<>();
 
         Scanner s = new Scanner(System.in);
@@ -26,8 +29,9 @@ public class Duke {
             } else if (in.equals("list")) {
                 System.out.println("     Here are the tasks in your list:");
                 for (int i = 0; i < al.size(); i++) {
+                    char t = typ.get(i) == TaskType.T ? 'T' : typ.get(i) == TaskType.D ? 'D' : 'E';
                     System.out.printf("     %d.[%c][%s] %s%s\n",
-                            i + 1, typ.get(i), al2.get(i) ? "X" : " ", al.get(i), det.get(i));
+                            i + 1, t, al2.get(i) ? "X" : " ", al.get(i), det.get(i));
                 }
 
             } else if (in.startsWith("mark ")) {
@@ -35,8 +39,9 @@ public class Duke {
                     int n = Integer.parseInt(in.split(" ")[1]) - 1;
                     if (n < 0 || n >= al.size()) throw new IndexOutOfBoundsException();
                     al2.set(n, true);
+                    char t = typ.get(n) == TaskType.T ? 'T' : typ.get(n) == TaskType.D ? 'D' : 'E';
                     System.out.println("     Nice! I've marked this task as done:");
-                    System.out.printf("       [%c][X] %s%s\n", typ.get(n), al.get(n), det.get(n));
+                    System.out.printf("       [%c][X] %s%s\n", t, al.get(n), det.get(n));
                 } catch (Exception e) {
                     System.out.println("     OOPS!!! Please provide a valid task number to mark.");
                 }
@@ -46,8 +51,9 @@ public class Duke {
                     int n = Integer.parseInt(in.split(" ")[1]) - 1;
                     if (n < 0 || n >= al.size()) throw new IndexOutOfBoundsException();
                     al2.set(n, false);
+                    char t = typ.get(n) == TaskType.T ? 'T' : typ.get(n) == TaskType.D ? 'D' : 'E';
                     System.out.println("     OK, I've marked this task as not done yet:");
-                    System.out.printf("       [%c][ ] %s%s\n", typ.get(n), al.get(n), det.get(n));
+                    System.out.printf("       [%c][ ] %s%s\n", t, al.get(n), det.get(n));
                 } catch (Exception e) {
                     System.out.println("     OOPS!!! Please provide a valid task number to unmark.");
                 }
@@ -56,9 +62,10 @@ public class Duke {
                 try {
                     int n = Integer.parseInt(in.split(" ")[1]) - 1;
                     if (n < 0 || n >= al.size()) throw new IndexOutOfBoundsException();
+                    char t = typ.get(n) == TaskType.T ? 'T' : typ.get(n) == TaskType.D ? 'D' : 'E';
                     System.out.println("     Noted. I've removed this task:");
                     System.out.printf("       [%c][%s] %s%s\n",
-                            typ.get(n), al2.get(n) ? "X" : " ", al.get(n), det.get(n));
+                            t, al2.get(n) ? "X" : " ", al.get(n), det.get(n));
                     al.remove(n); al2.remove(n); typ.remove(n); det.remove(n);
                     System.out.printf("     Now you have %d tasks in the list.\n", al.size());
                 } catch (Exception e) {
@@ -70,7 +77,7 @@ public class Duke {
                 if (desc.isEmpty()) {
                     System.out.println("     OOPS!!! The description of a todo cannot be empty.");
                 } else {
-                    al.add(desc); al2.add(false); typ.add('T'); det.add("");
+                    al.add(desc); al2.add(false); typ.add(TaskType.T); det.add("");
                     System.out.println("     Got it. I've added this task:");
                     System.out.printf("       [T][ ] %s\n", desc);
                     System.out.printf("     Now you have %d tasks in the list.\n", al.size());
@@ -87,7 +94,7 @@ public class Duke {
                     if (desc.isEmpty() || by.isEmpty()) {
                         System.out.println("     OOPS!!! Deadline needs both description and /by.");
                     } else {
-                        al.add(desc); al2.add(false); typ.add('D');
+                        al.add(desc); al2.add(false); typ.add(TaskType.D);
                         det.add(" (by: " + by + ")");
                         System.out.println("     Got it. I've added this task:");
                         System.out.printf("       [D][ ] %s (by: %s)\n", desc, by);
@@ -108,7 +115,7 @@ public class Duke {
                     if (desc.isEmpty() || from.isEmpty() || to.isEmpty()) {
                         System.out.println("     OOPS!!! Event needs description, /from and /to.");
                     } else {
-                        al.add(desc); al2.add(false); typ.add('E');
+                        al.add(desc); al2.add(false); typ.add(TaskType.E);
                         String d = " (from: " + from + " to: " + to + ")";
                         det.add(d);
                         System.out.println("     Got it. I've added this task:");
