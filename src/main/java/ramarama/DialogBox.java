e6eef05c0bb8e9d7302babc22784285181ea7a94
Chieu Le Heng <chieuleheng@gmail.com>
2025-09-07 11:27:14 +0800

    Add GUI

---
+++ src/main/java/ramarama/DialogBox.java
@@ -0,0 +1,43 @@
+package ramarama;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
+public class DialogBox extends HBox {
+    @FXML private Label text;
+    @FXML private ImageView displayPicture;
+
+    private DialogBox(String msg, Image img) {
+        try {
+            FXMLLoader fxml = new FXMLLoader(getClass().getResource("/view/DialogBox.fxml"));
+            fxml.setRoot(this);
+            fxml.setController(this);
+            fxml.load();                   // <-- this populates text & displayPicture
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        text.setText(msg);
+        displayPicture.setImage(img);
+    }
+
+    public static DialogBox getUserDialog(String msg, Image img) {
+        DialogBox db = new DialogBox(msg, img);
+        db.flip();                         // now safe
+        return db;
+    }
+
+    public static DialogBox getRama2Dialog(String msg, Image img) {
+        return new DialogBox(msg, img);
+    }
+
+    private void flip() {
+        var nodes = javafx.collections.FXCollections.observableArrayList(getChildren());
+        java.util.Collections.reverse(nodes);
+        getChildren().setAll(nodes);
+        setAlignment(javafx.geometry.Pos.TOP_RIGHT);
+    }
+}
---
+++ src/main/java/ramarama/Main.java
@@ -0,0 +1,27 @@
+package ramarama;
+
+import java.nio.file.Paths;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        System.out.println("jh\n\n\n");
+        Rama2 rama2 = new Rama2(Paths.get("data", "rama.txt").toString());
+        System.out.println("FXML URL = " + Main.class.getResource("/view/MainWindow.fxml"));
+        FXMLLoader fxml = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+        
+
+        Scene scene = new Scene(fxml.load());
+        MainWindow controller = fxml.getController();
+        controller.setRama2(rama2);
+        stage.setTitle("Rama2");
+        stage.setScene(scene);
+        stage.show();
+        controller.greet();
+    }
+}
